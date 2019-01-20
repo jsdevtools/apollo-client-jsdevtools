8999a8d7369cfae3b4e01a895a393674a207ffd2
Justin Staubach <justin@staubach.us>
2019-01-20 17:09:00 -0500

    Point to dev server release

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: process.env.SPACE_API_HOST && process.env.SPACE_API_ENDPOINT && process.env.PORT
+    ? `${process.env.SPACE_API_HOST}:${process.env.PORT}/${process.env.SPACE_API_ENDPOINT}`
+    : 'http://localhost:4000/graphql',
     headers: {
       authorization: localStorage.getItem('token'),
       'client-name': 'Space Explorer [web]',
