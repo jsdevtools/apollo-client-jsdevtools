3ddb8701f9f865cd3a7086b9553f30e89d39797d
Justin Staubach <justin@staubach.us>
2019-01-29 22:01:08 -0500

    explicitly set port, hope that helps.

--- src/index.js
+++ src/index.js
@@ -18,10 +18,9 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri:
-      process.env.SPACE_API_HOST && process.env.SPACE_API_ENDPOINT
-        ? `${process.env.SPACE_API_HOST}/${process.env.SPACE_API_ENDPOINT}`
-        : 'http://localtest:4000/graphql',
+    uri: process.env.SPACE_PORT
+      ? `${process.env.SPACE_API_HOST}/${process.env.SPACE_API_ENDPOINT}`
+      : 'http://localhost:4000/graphql',
     headers: {
       authorization: localStorage.getItem('token'),
       'client-name': 'Space Explorer [web]',
