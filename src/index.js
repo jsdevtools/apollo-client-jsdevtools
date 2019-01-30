d00c9009c61e66c572f4825a51aedb8fd8b7f703
Justin Staubach <justin@staubach.us>
2019-01-29 21:47:16 -0500

    test changing the uri and see if it shows up in logs.

--- src/index.js
+++ src/index.js
@@ -18,9 +18,10 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: process.env.PORT
-      ? `${process.env.SPACE_API_HOST}/${process.env.SPACE_API_ENDPOINT}`
-      : 'http://localhost:4000/graphql',
+    uri:
+      process.env.SPACE_API_HOST && process.env.SPACE_API_ENDPOINT
+        ? `${process.env.SPACE_API_HOST}/${process.env.SPACE_API_ENDPOINT}`
+        : 'http://localtest:4000/graphql',
     headers: {
       authorization: localStorage.getItem('token'),
       'client-name': 'Space Explorer [web]',
